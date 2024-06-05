86145900331c5f75ff385b72879f7d8deac4df52
Jay <wmeng0103@gmail.com>
2024-06-05 14:42:03 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Wayne-resource
+https://union-center-zeta.vercel.app/contact
+
+This website is my Front-End production.
+Please check carefully
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  
+  return (
+    <>
+      <div className="w-full flex text-center bg-main-bg h-[100vh]">
+        <p className="text-5xl w-full text-center mt-28">Wayne Resource Server</p>
+      </div>
+    </>
+    
+  );
+}
+
+export default App;
